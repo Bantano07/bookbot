1337d30fe6ee8c1a3191de6e2b2ed64cf631aedf
Bantano07 <28aoun.johnpaul@wollemi.nsw.edu.au>
2026-09-17 09:30:18 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import get_word_count, get_char_count
+
 def get_book_text(path_to_file: str) -> str:
     with open(path_to_file) as f:
         file_contents = f.read()
@@ -8,8 +10,5 @@ def main():
 
 book_contents = get_book_text("books/frankenstein.txt")
 
-def word_count(book: str) -> int:
-    words = len(book.split())
-    print(f"Found {words} total words")
-
-word_count(book_contents)
+print(get_word_count(book_contents))
+print(get_char_count(book_contents))                                                            
