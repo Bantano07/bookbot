8fd28959284e1489ae6afb20120b16e065e78c7d
Bantano07 <28aoun.johnpaul@wollemi.nsw.edu.au>
2026-09-20 17:36:25 +1000

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 import sys
 from stats import get_word_count, get_char_count, chars_dict_to_sorted_list
+#Imports functions from stats.py file
 
 if len(sys.argv) < 2:
+    # CHecks if user input a file
     print("Usage: python3 main.py <path_to_book>")
     sys.exit(1)
 
 book_path = sys.argv[1]
 
 def get_book_text(path_to_file: str) -> str:
+    # File contents converted to a string
     with open(path_to_file) as f:
         file_contents = f.read()
         return file_contents
 
 def print_report(book_path: str, word_count: int, sorted_char_count: list[tuple[str, int]]):
+    # Final output logged to console
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
@@ -21,8 +25,9 @@ def print_report(book_path: str, word_count: int, sorted_char_count: list[tuple[
 
     if sorted_char_count == []:
         print("Found 0 total characters")
-        
+
     for char in sorted_char_count:
+        # Skips non-alphanumeric characters
         if char[0].isalpha():
             print(f"{char[0]}: {char[1]}")
 
@@ -33,4 +38,5 @@ word_count = get_word_count(book_contents)
 char_count = get_char_count(book_contents)
 sorted_char_count = chars_dict_to_sorted_list(char_count)
 
+# Final function call
 print_report(book_path, word_count, sorted_char_count)
--- stats.py
+++ stats.py
@@ -1,13 +1,22 @@
 def get_word_count(book: str) -> int:
+    # Count the number of words in the book
     words = len(book.split())
+    
+    # Return the word count
     if words == 1:
         return f"Found 1 total word"
     else: 
         return f"Found {words} total words"
 
+
 def get_char_count(book: str) -> dict[str, int]:
+    # Convert all characters to lowercase
     book_chars = book.lower()
+    
+    # Create a dictionary to store character counts
     char_count_dict = {}
+    
+    # Go through each character and count how often it appears
     for char in book_chars:
         if char not in char_count_dict:
             char_count_dict[char] = 1
@@ -16,13 +25,20 @@ def get_char_count(book: str) -> dict[str, int]:
         
     return char_count_dict
 
+
 def sort_on(char_tuple: tuple[str, int]) -> int:
+    # Use the character count for sorting
     return char_tuple[1]
 
+
 def chars_dict_to_sorted_list(char_count_dict: dict[str, int]) -> list[tuple[str, int]]:
+    # Create a list from the character dictionary
     unsorted_list = []
+    
     for char in char_count_dict:
         unsorted_list.append((char, char_count_dict[char]))
     
+    # Sort characters from highest to lowest count
     sorted_list = sorted(unsorted_list, reverse=True, key=sort_on)
+    
     return sorted_list
